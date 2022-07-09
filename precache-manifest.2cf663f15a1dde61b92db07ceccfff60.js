2c5d9163b46273ce7a1fa281d7fd04e0cc96269f
Studio Fomalhaut <studio@fomalhaut.nl>
2022-07-09 23:25:27 +0200

    Updates

---
+++ precache-manifest.2cf663f15a1dde61b92db07ceccfff60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e42730f599216fc4e269",
+    "url": "/MonstersRolox/static/css/main.9c11d1d1.chunk.css"
+  },
+  {
+    "revision": "e42730f599216fc4e269",
+    "url": "/MonstersRolox/static/js/main.a4d9f7aa.chunk.js"
+  },
+  {
+    "revision": "452f5555d598d63cc2b2",
+    "url": "/MonstersRolox/static/js/runtime~main.58592f62.js"
+  },
+  {
+    "revision": "d7552a1f1f052f900238",
+    "url": "/MonstersRolox/static/js/2.265c2a91.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/MonstersRolox/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/MonstersRolox/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "cca34194dad8d41762e996eca333e44c",
+    "url": "/MonstersRolox/index.html"
+  }
+];
